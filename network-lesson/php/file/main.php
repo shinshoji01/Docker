8af31b6e1966a84ae6277c0b610732a17792a750
Sho Inoue <shoshoinoue@icloud.com>
2021-07-06 01:26:43 +0900

    finish the layout

--- network-lesson/php/file/main.php
+++ network-lesson/php/file/main.php
@@ -15,6 +15,8 @@ This website contains the demo of a text-to-speech conversion system using deepl
 <div class="wrapper">
     <div> 
         <br>
+        <br>
+        <br>
         <p> <input type="text" name="textfile"/> </p>
         <p> <img src="./images/pencil_eraser_colored.png" alt="pencil and eraser" title="pencil and eraser" width="200"> </p>
     </div>
@@ -28,16 +30,18 @@ This website contains the demo of a text-to-speech conversion system using deepl
     <div> 
         <?php
         if (!isset($_POST['generate'])){
-            displayAudio("audio/shoshoinoue.wav");
+            $text = "shoshoinoue";
+            $filename = text2filename($text);
+            echo "<center><font size='5' face='ＭＳ ゴシック'> ".$text." </font></center>";
+            displayAudio($filename);
         }
         ?>
         <?php
         if (isset($_POST['generate'])){
-            $dir='audio/';
             $text = $_POST["textfile"];
-            $filename = strtolower($text);
-            $filename = str_replace(" ", "_", $filename);
-            $filename = $dir.$filename.".wav";
+            echo "<center>".$text."</center>";
+            echo "<br>";
+            $filename = text2filename($text);
             $found = findAudio($filename);
             if(!$found){ # no file in the database
                 #echo "getting data";
@@ -55,20 +59,107 @@ This website contains the demo of a text-to-speech conversion system using deepl
                 saveAudio($filename);
             }
             #echo $_POST['textfile'];
-            displayAudio($filename);
+            displayAudio($text);
         }
         ?>
 
         <p> <img src="./images/singing_colored.png" alt="singing" title="singing" width="200"> </p>
         
     </div>
+    <div> </div>
+    <!-- <div><p><font size="6">Examples</font></p></div> -->
+    <div><h1>Examples</h1></div>
+    <div> </div>
+    <!-- 1st example-->
+    <div>
+        <?php $text = "Please, wash your hands before eating." ?>
+        <br>
+        <div class=area>
+            <?php echo "<p><font size='5' face='ＭＳ ゴシック'>".$text."</font></p>"?>
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <img src="./images/wash_hands.png" alt="wash_hands" title="wash_hands" width="100">
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <?php displayAudio(text2filename($text))?>
+        </div>
+    </div>
+    <!-- 2nd example-->
+    <div>
+        <?php $text = "Mt. Fuji is the highest mountain in Japan." ?>
+        <br>
+        <div class=area>
+            <?php echo "<p><font size='5' face='ＭＳ ゴシック'>".$text."</font></p>"?>
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <img src="./images/mtfuji_colored.png" alt="mtfuji" title="mtfuji" width="100">
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <?php displayAudio(text2filename($text))?>
+        </div>
+    </div>
+    <!-- 3rd example-->
+    <div>
+        <?php $text = "Tokyo Skytree is over three hundred taller than Tokyo Tower." ?>
+        <br>
+        <div class=area>
+            <?php echo "<p><font size='5' face='ＭＳ ゴシック'>".$text."</font></p>"?>
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <img src="./images/skytree.png" alt="skytree" title="skytree" width="100">
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <?php displayAudio(text2filename($text))?>
+        </div>
+    </div>
+    <!-- 4th example-->
+    <div>
+        <?php $text = "A recent innivation that helps people in the world." ?>
+        <br>
+        <div class=area>
+            <?php echo "<p><font size='5' face='ＭＳ ゴシック'>".$text."</font></p>"?>
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <img src="./images/online_chatting.png" alt="online_chatting" title="online_chatting" width="100">
+        </div>
+    </div>
+    <div>
+        <div class=area>
+        <?php displayAudio(text2filename($text))?>
+        </div>
+    </div>
 </div>
+
+
 
 </body>
 </html>
 <?php
-function displayAudio($filename)
+function text2filename($text)
 {
+    $dir='audio/';
+    $filename = strtolower($text);
+    $filename = str_replace(" ", "_", $filename);
+    $filename = $dir.$filename.".wav";
+    return $filename;
+}
+function displayAudio($text)
+{
+    $filename = text2filename($text)
     ?>
     <p>
     <audio controls>
