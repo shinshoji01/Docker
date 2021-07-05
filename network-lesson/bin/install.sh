31ccb79b0e98b01fd29f3c13ceb29501f75da407
shinshoji01 <shoshoinoue@gmail.com>
2021-07-05 20:10:14 +0900

    finish overall idea

---
+++ network-lesson/bin/install.sh
@@ -0,0 +1,10 @@
+docker build -f python/docker/Dockerfile -t python-initialization:latest python/docker/
+docker run --name python_init --gpus all -it -v $PWD/python/file/:/work -v $PWD/python/file/cache/:/work/cache/ -v $PWD/python/file/parameters/:/work/parameters/ python-initialization python3 /work/initialize.py
+docker restart python_init
+docker exec -it python_init cp -r /root/.cache/torch/hub/nvidia_DeepLearningExamples_torchhub/ /work/cache/
+docker restart python_init
+docker exec -it python_init cp -r /root/.cache/torch/hub/NVIDIA_DeepLearningExamples_torchhub/ /work/cache/
+docker stop python_init
+
+mkdir db/mysql/
+cp bin/mysql_initial/* db/mysql/*
--- network-lesson/python/file/get_audio.py
+++ network-lesson/python/file/get_audio.py
@@ -1,6 +1,8 @@
 import numpy as np
 import soundfile as sf
 import configargparse
+import torch
+from WaveGlow import call_waveglow
 import time
 
 def main():
@@ -9,18 +11,35 @@ def main():
     parser.add("--text", type=str, required=True, help="text that you like to convert")
     parser.add("--rate", type=int, default=22050, help="sample rate")
     parser.add("--save_dir", type=str, default="/work/audio/")
+    parser.add("--waveglow_param_dir", type=str, default="/work/parameters/")
     args = parser.parse_args()
     text = args.text
     rate = args.rate
     save_dir = args.save_dir
-
+    waveglow_param_dir = args.waveglow_param_dir
+    
+    # preparation
+    ## waveglow
+    waveglow = call_waveglow(waveglow_param_dir+"waveglow_official.pth")
+    waveglow = waveglow.to('cuda')
+    waveglow.eval()
+    ## tacotron2
+    tacotron2 = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_tacotron2')
+    tacotron2 = tacotron2.to('cuda')
+    tacotron2.eval()
+    ## text preprocessing
     input_text = text.lower()
-    np.random.seed(len(input_text))
-    audio = np.random.randn(int(rate*1.5))
+    
+    # get audio
+    utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tts_utils')
+    sequences, lengths = utils.prepare_input_sequence([input_text])
+    with torch.no_grad():
+        mel, _, _ = tacotron2.infer(sequences, lengths)
+        audio = waveglow.infer(mel)
+    audio = audio[0].data.cpu().numpy()[0]
 
     path = save_dir + f"{input_text.replace(' ', '_')}.wav"
     sf.write(path, audio, rate, subtype='PCM_24')
-    time.sleep(3)
     
 if __name__ == "__main__":
     main()
